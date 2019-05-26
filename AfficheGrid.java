41c70753f29501db3016fdc0762ed489118ad751
Appollo17 <50587075+Appollo17@users.noreply.github.com>
2019-05-26 18:25:02 +0200

    Add files via upload
    Problème avec un point virgule....

---
+++ AfficheGrid.java
@@ -0,0 +1,79 @@
+package Vue;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+import Vue.HexagonMeriem;
+import Vue.HexagonMeriem.Hexagon;
+import java.awt.EventQueue;
+
+
+//import java.awt.Color;
+
+public class AfficheGrid {
+
+	/*public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Menu frame = new Menu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		//Interface I =new Interface();
+	}*/
+
+
+		 	final int width = 50;
+	        final int height = 50;
+	        final Hexagon[][] grid = new Hexagon[height][width];
+	        for(int row = 0; row < height; row++) {
+	            for(int col = 0; col < width; col++) {
+	                grid[row][col] = new Hexagon(row, col, 50);
+	            }
+	        }
+	
+	        JFrame f = new JFrame("Hexagons");
+	        f.getContentPane().setLayout(new GridLayout());
+	        f.getContentPane().add(new JComponent() {
+	          public void paint(Graphics g) {
+	            g.setColor(new Color(0xFF, 0xFF, 0xFF));
+	            //g.fillRect(0,0,getWidth(),getHeight());
+	            g.setColor(new Color(0,0,0));
+	            final int[] xs = new int[6];
+	            final int[] ys = new int[6];
+	            for (Hexagon[] row : grid) {
+	              for (Hexagon h: row) {
+	                final int[] i = {0};
+	                h.foreachVertex((x, y) -> {
+	                  xs[i[0]] = (int)((double)x);
+	                  ys[i[0]] = (int)((double)y);
+	                  i[0]++;
+	                });
+	                g.drawPolygon(xs, ys, 6);
+	
+	              
+	              }
+	            }
+	          }
+	        });
+	        f.setBounds(0, 0, 500, 500);
+	        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        f.setVisible(true);
+	        try {
+	            Thread.sleep(100);
+	        } catch (Throwable e) {
+	
+	        } finally {
+	            f.repaint();
+	        }
+	    }
+}
+	
