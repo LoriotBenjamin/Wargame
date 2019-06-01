2f9d462e38a1910d431e937bf85482b7c379c7c0
Solenn <Solenn@SOLENN>
2019-06-01 21:30:21 +0200

    JAVADOC IA

--- src/modele/IA.java
+++ src/modele/IA.java
@@ -30,59 +30,59 @@ public class IA extends Joueur {
     @Override
     public void jouerTour() {
         for (Unite unite : this.getListeUnite()) {
-            boolean attaque = false;
             MyHashMap<Hexagone, Integer> deplacementPossible = unite.calculDeplacementPossible();
             Iterator<Map.Entry<Hexagone, Integer>> iterator = deplacementPossible.entrySet().iterator();
 
-            while (!attaque && iterator.hasNext()) {
+            recherche: while (iterator.hasNext()) {
                 Map.Entry<Hexagone, Integer> mapEntry = iterator.next();
                 Hexagone hexagone = mapEntry.getKey();
 
-                recherche: for (Joueur joueur : Jeu.getListeJoueurs()) {
+                for (Joueur joueur : Jeu.getListeJoueurs()) {
                     if (joueur != this) {
                         for (Unite uniteAdverse : joueur.getListeUnite()) {
                             if (unite.getTypeUnite() != Jeu.PRETRE && hexagone.getDistanceBetweenTwoPosition(
-                                            Jeu.getMap()[uniteAdverse.getX()][uniteAdverse.getY()] ) <= unite.getPorte()) {
+                                    Jeu.getMap()[uniteAdverse.getX()][uniteAdverse.getY()]) <= unite.getPorte()) {
                                 // unité à portée d'attaque trouvée
                                 unite.setX(hexagone.getX());
                                 unite.setY(hexagone.getY());
                                 unite.setPtDeDeplacement((Integer) mapEntry.getValue());
                                 Jeu.affichageUnite();
+
                                 System.out.println("Attaque de " + joueur.getPseudo());
                                 System.out.println(unite);
                                 System.out.println("sur " + uniteAdverse);
+
                                 unite.attaquer(uniteAdverse);
+
                                 System.out.println("Résultat :");
                                 System.out.println(unite);
                                 System.out.println(uniteAdverse);
 
-                                attaque = true;
-
                                 deplacementPossible = unite.calculDeplacementPossible();
-                                
+
                                 break recherche;
 
                             }
                         }
-                    } 
+                    }
                 }
             }
-            
-            if(unite.getTypeUnite() == Jeu.PRETRE) {
-                for(Unite uniteAlliee : listeUnite) {
-                    if(uniteAlliee != unite && uniteAlliee.getPv() != uniteAlliee.getPvMax() 
+
+            if (unite.getTypeUnite() == Jeu.PRETRE) {
+                for (Unite uniteAlliee : listeUnite) {
+                    if (uniteAlliee != unite && uniteAlliee.getPv() != uniteAlliee.getPvMax()
                             && Jeu.getMap()[unite.getX()][unite.getY()].getDistanceBetweenTwoPosition(
                                     Jeu.getMap()[uniteAlliee.getX()][uniteAlliee.getY()]) <= unite.getPorte()) {
-                        //soigne unités alliées à portée
+                        // soigne unités alliées à portée
                         System.out.println(unite + "\nsoigne" + "\n" + uniteAlliee);
                         unite.soigner(uniteAlliee);
                     }
                 }
             }
-            
+
             final double pourcentagePV = 0.75;
-   
-            if (!deplacementPossible.isEmpty() &&  unite.getPv() >= (int) (pourcentagePV * unite.getPvMax())) {
+
+            if (!deplacementPossible.isEmpty() && unite.getPv() >= (int) (pourcentagePV * unite.getPvMax())) {
                 // si deplacement possible et plus de 75% de PV restant alors déplacement aléatoire
 
                 Object[] tabDeplacementPossible = deplacementPossible.keySet().toArray();
@@ -92,11 +92,13 @@ public class IA extends Joueur {
                 unite.setY(hexagone.getY());
                 unite.setPtDeDeplacement((Integer) deplacementPossible.get(key));
                 Jeu.affichageUnite();
+
                 System.out.println("Déplacement sans attaquer:");
                 System.out.println(unite);
-            } 
-            
+            }
+
         }
+
         for (Unite unite : this.getListeUnite()) {
             unite.preparerPourProchainTour();
         }
