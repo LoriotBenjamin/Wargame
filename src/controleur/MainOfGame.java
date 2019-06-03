5ecb95512b34911ff313525f88404ad2282e6da3
Solenn <Solenn@SOLENN>
2019-06-03 13:22:59 +0200

    JAVADOC MAIN

--- src/controleur/MainOfGame.java
+++ src/controleur/MainOfGame.java
@@ -4,42 +4,52 @@ import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
-import modele.Joueur; // à retirer d'ici 
+import modele.Joueur;
 import vue.Menu;
 
+/**
+ * Classe qui lance le main du programme.
+ * @author Solenn
+ *
+ */
 public class MainOfGame {
 
-	public static void main(String[] args) {
-		totality : do {
-    		Menu menu = new Menu();
-    		Joueur winner = null;
-    		do {
-    			System.out.println("HELLO");
-    			do {System.out.print("");}while(Jeu.hasStarted() == false);
-    			System.out.println("YOHOHOHO");
-    			//Jeu.chargerPartie("save");
-    			Jeu.affichageUnite();
-    			
-    			do {
-    				ArrayList<Joueur> perdant = new ArrayList<Joueur>();
-    				Joueur joueur = Jeu.getListeJoueurs().get(Jeu.getTurn()-1); //tour des joueurs chacun leur tour
-    			    joueur.jouerTour();
-    			    if(!Jeu.hasStarted())
-    			    	continue totality;
-    			    if(joueur.getListeUnite().size() == 0) { //mise en mémoire des perdants
-    			        perdant.add(joueur);
-    			    }
-    			    if(!perdant.isEmpty()) {//on enlève les perdants de la liste
-    			        Jeu.getListeJoueurs().removeAll(perdant);
-    			    }
-    			    Jeu.nextTurn();
-    			}while(Jeu.getListeJoueurs().size() > 1 && Jeu.hasStarted());
-    			Jeu.affichageUnite();
-    			winner = Jeu.getListeJoueurs().get(0);
-    		}while(winner == null);
+    /**
+     * Fonction main du programme.
+     * @param args Arguments de lancement.
+     */
+    public static void main(final String[] args) {
+        totality: do {
+            Menu menu = new Menu();
+            Joueur winner = null;
+            do {
+                do {
+                    System.out.print("");
+                } while (!Jeu.hasStarted());
+                Jeu.affichageUnite();
+
+                do {
+                    ArrayList<Joueur> perdant = new ArrayList<Joueur>();
+                    Joueur joueur = Jeu.getListeJoueurs().get(Jeu.getTurn() - 1); // tour des joueurs chacun leur tour
+                    joueur.jouerTour();
+                    if (!Jeu.hasStarted()) {
+                        continue totality;
+                    }
+                    if (joueur.getListeUnite().size() == 0) { // mise en mémoire des perdants
+                        perdant.add(joueur);
+                    }
+                    if (!perdant.isEmpty()) { //on enlève les perdants de la liste
+                        Jeu.getListeJoueurs().removeAll(perdant);
+                    }
+                    Jeu.nextTurn();
+                } while (Jeu.getListeJoueurs().size() > 1 && Jeu.hasStarted());
+                Jeu.affichageUnite();
+                winner = Jeu.getListeJoueurs().get(0);
+            } while (winner == null);
             Jeu.kill();
-            JOptionPane.showMessageDialog(null, "Fin de la partie.\n"+winner.getPseudo() + " a gagné.", "Fin de la partie", JOptionPane.INFORMATION_MESSAGE);
-		} while(true);
-	}
+            JOptionPane.showMessageDialog(null, "Fin de la partie.\n" + winner.getPseudo() + " a gagné.",
+                    "Fin de la partie", JOptionPane.INFORMATION_MESSAGE);
+        } while (true);
+    }
 
 }
